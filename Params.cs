39a1f1f98cde600eaa9d2a15c12d8b8cc1ee2b17
s0ur0u <96497609+s0ur0u@users.noreply.github.com>
2022-07-08 13:27:32 -0400

    Add files via upload

---
+++ Params.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_C_Sharp
+{
+    /*1*/
+    public class Reversing
+    {
+        public int[] GenerateNumbers()
+        {
+            Console.WriteLine("Please enter numbers separated by commas");
+            int[] numbs = new int[10];
+            string numbers = Console.ReadLine();
+            string[] number = numbers.Split(',');
+            for(int i=0; i < number.Length; ++i)
+            {
+                numbs[i] = Convert.ToInt32(number[i]);
+            }
+            return numbs;
+
+        }
+        public int[] Reverse(int[] arr)
+        {
+            int temp;
+            for (int i=1; i<arr.Length; i++)
+            {
+                temp = arr[i-1];
+                arr[i-1] = arr[i];
+                arr[i] = temp;
+            }
+            return arr;
+
+        }
+        public void PrintNumbers(int[]array)
+        {
+            foreach(int i in array)
+            {
+                Console.Write(i+", ");
+            }
+        }
+    }
+    public class Fibonacci
+    {
+        public void Sequence(int number)
+        {
+            int value = 11;
+            int[]arr = new int[value];
+            for(int i = 2; i < value; i++)
+            {
+                arr[0] = 1;
+                arr[1] = 1;
+                arr[i] = arr[i - 1] + arr[i-2];
+            }
+            Console.WriteLine(arr[number-1]);
+        }
+    }
+}
+
+/*Designing and Building Classes using object-oriented principles*/
+
+//2-
+public abstract class Person
+{
+    public abstract string Name();
+}
+
+public class Student : Person
+{
+    public override string Name()
+    {
+        return "Mikey";
+    }
+    public string Greet()
+    {
+        return "Hi my name is " + Name + " I am a student.";
+    }
+}
+
+public class Instructor : Person
+{
+    public override string Name()
+    {
+        return "Jason";
+    }
+
+    public string Greet2()
+    {
+        return "Hi I am " + Name + " and I am an instructor";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿/*1-Let’s make a program that uses methods to accomplish a task. Let’s take an array and
+reverse the contents of it. */
+
+namespace Assignment3_C_Sharp;
+//public class Reverse
+//{
+//    public static void Main(string[] args)
+//    {
+//        Reversing p = new Reversing();
+//        int[] numbers = p.GenerateNumbers();
+//        int[] number = p.Reverse(numbers);
+//        p.PrintNumbers(number);
+//    }
+//}
+
+///*2-Fibonacci Sequence*/
+//public class FibonacciSequence
+//{
+//    public static void Main(string[] args)
+//    {
+//        Console.WriteLine("Please enter the nth element out of the 10 Fibonacci Sequence: ");
+//        int element = int.Parse(Console.ReadLine());
+//        Fibonacci f = new Fibonacci();
+//        f.Sequence(element);
+//    }
+//}
+
+
+/*Designing and Building Classes using object-oriented principles*/
+
+/*1-Write a program that that demonstrates use of four basic principles of
+object-oriented programming /Abstraction/, /Encapsulation/, /Inheritance/ and
+/Polymorphism/.*/
+
+//Ecapsulation
+public class Encap
+{
+    public int Example1 { get; set; }
+
+}
+
+//Inheritance
+public class Employee
+{
+    public int EmployeeID { get; set; }
+    public string Name { get; set; }
+}
+public class FullTime : Employee
+{
+    public FullTime(int id, string name)
+    {
+        EmployeeID = id;
+        Name = name;
+    }
+}
+
+//Abstraction
+public abstract class Shape
+{
+    public abstract int Area();
+}
+
+public class Square : Shape
+{
+    private int side;
+    public Square(int x = 0)
+    {
+         x = side;
+    }
+    public override int Area()
+    {
+        return side * side;
+    }
+}
+
+
+/*2-Use /Abstraction/ to define different classes for each person type such as Student
+and Instructor. These classes should have behavior for that type of person*/
+
+public class Abstraction
+{
+    public static void Main(string[] args)
+    {
+        Student s = new Student();
+        Console.WriteLine(s.Greet());
+        Console.WriteLine();
+        Instructor i = new Instructor();
+        Console.WriteLine(i.Greet2);
+    }
+}
