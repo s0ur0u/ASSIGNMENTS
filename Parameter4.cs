d5b9de2ea44f3c429f6d4fcd138b7abe955b989e
s0ur0u <96497609+s0ur0u@users.noreply.github.com>
2022-07-09 10:49:44 -0400

    Add files via upload

---
+++ Parameter4.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4_C_Sharp
+{
+    /*1-*/
+    public class MyStack<T>
+    {
+        int capacity;
+        T[] stack;
+        int top;
+
+        public MyStack(int maxElements)
+        {
+            capacity = maxElements;
+            stack = new T[capacity];
+
+        }
+
+        public int Push(T element)
+        {
+            if (top == capacity - 1)
+            {
+                return -1;
+            }
+            else
+            {
+                top = top + 1;
+                stack[top] = element;
+            }
+            return 0;
+        }
+
+        public T Pop()
+        {
+            T removedElement;
+            T temp = default(T);
+
+            if (!(top <= 0))
+            {
+                removedElement = stack[top];
+                top = top - 1;
+                return removedElement;
+            }
+            return temp;
+        }
+
+        public T[] Count()
+        {
+            T[] elements = new T[top + 1];
+            Array.Copy(stack, 0, elements, 0, top+1);
+            return elements;
+        }
+    }
+    /*2-*/
+    public class MyList<T>
+    {
+        
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,90 +1,83 @@
-﻿/*1-Let’s make a program that uses methods to accomplish a task. Let’s take an array and
-reverse the contents of it. */
-
-namespace Assignment3_C_Sharp;
-//public class Reverse
-//{
-//    public static void Main(string[] args)
-//    {
-//        Reversing p = new Reversing();
-//        int[] numbers = p.GenerateNumbers();
-//        int[] number = p.Reverse(numbers);
-//        p.PrintNumbers(number);
-//    }
-//}
-
-///*2-Fibonacci Sequence*/
-//public class FibonacciSequence
-//{
-//    public static void Main(string[] args)
-//    {
-//        Console.WriteLine("Please enter the nth element out of the 10 Fibonacci Sequence: ");
-//        int element = int.Parse(Console.ReadLine());
-//        Fibonacci f = new Fibonacci();
-//        f.Sequence(element);
-//    }
-//}
-
-
-/*Designing and Building Classes using object-oriented principles*/
-
-/*1-Write a program that that demonstrates use of four basic principles of
-object-oriented programming /Abstraction/, /Encapsulation/, /Inheritance/ and
-/Polymorphism/.*/
-
-//Ecapsulation
-public class Encap
-{
-    public int Example1 { get; set; }
-
-}
-
-//Inheritance
-public class Employee
-{
-    public int EmployeeID { get; set; }
-    public string Name { get; set; }
-}
-public class FullTime : Employee
-{
-    public FullTime(int id, string name)
-    {
-        EmployeeID = id;
-        Name = name;
-    }
-}
-
-//Abstraction
-public abstract class Shape
-{
-    public abstract int Area();
-}
-
-public class Square : Shape
-{
-    private int side;
-    public Square(int x = 0)
-    {
-         x = side;
-    }
-    public override int Area()
-    {
-        return side * side;
-    }
-}
-
-
-/*2-Use /Abstraction/ to define different classes for each person type such as Student
-and Instructor. These classes should have behavior for that type of person*/
-
-public class Abstraction
+﻿namespace Assignment4_C_Sharp;
+/*1-Create a custom Stack class MyStack<T> that can be used with any data type which
+has following method*/
+public class Stack
 {
     public static void Main(string[] args)
     {
-        Student s = new Student();
-        Console.WriteLine(s.Greet());
-        Console.WriteLine();
-        Instructor i = new Instructor();
-        Console.WriteLine(i.Greet2);
+        int capacity;
+        Console.WriteLine("Enter the capacity of your Stack: ");
+        capacity = int.Parse(Console.ReadLine());
+
+        MyStack<string> stack = new MyStack<string>(capacity);
+
+        while (true)
+        {
+            Console.WriteLine("1.Push");
+            Console.WriteLine("2.Pop");
+            Console.WriteLine("3.Print Stack Elements");
+            Console.WriteLine("4.Exit");
+
+            Console.WriteLine("Enter your choice: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Enter the string to push: ");
+                        string temp = Console.ReadLine();
+                        int result = stack.Push(temp);
+
+                        if(result != -1)
+                        {
+                            Console.WriteLine("Element pushed into the Stack !");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Stack Overflow !");
+                        }
+                        break;
+                    }
+                    case 2:
+                    {
+                        string Result = stack.Pop();
+                        if(Result != null)
+                        {
+                            Console.WriteLine("Delete Element: "+Result);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Stack Underflow !");
+                        }
+                        break ;
+                    }
+                    case 3:
+                    {
+                        string[] elements = stack.Count();
+                        Console.WriteLine("STACK CONTENT: ");
+                        foreach(string element in elements)
+                        {
+                            Console.WriteLine(element);
+                        }
+                        break;
+                    }
+                case 4:
+                    {
+                        System.Diagnostics.Process.GetCurrentProcess().Kill();
+                        break ;
+                    }
+                default:
+                    {
+                        Console.WriteLine("You have entered the wrong choice ");
+                        break;
+                    }
+            }
+        }
+
     }
+
 }
+/*2-Create a Generic List data structure MyList<T> that can store any data type.
+Implement the following methods.*/
+ 
